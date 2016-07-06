013cb0f4f7bf80a5d940412f7cf495e8076de09c
damiandee <damiandee02@gmail.com>
2016-07-06 12:27:47 +0200

    TET-6 test jednostkowy dla klasy Frame, do dokonczenia po mergu

--- src/test/java/tetris/view/FrameTest.java
+++ src/test/java/tetris/view/FrameTest.java
@@ -1,14 +1,26 @@
 package tetris.view;
 
+import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  * Created by Dominika on 2016-07-05.
  */
 public class FrameTest {
 
-    @Test
-    public void test1() {
+    //TODO dokonczyc test po mergu
+    Frame frame;
 
+    @Before
+    public void setUp() {
+        frame = new Frame();
+    }
+
+    @Test
+    public void checkStatusBarTest() {
+        String expectedStatus = "zpjj2016";
+        frame.statusbar.setText(expectedStatus);
+        assertEquals(frame.statusbar.getText(), expectedStatus);
     }
 }
